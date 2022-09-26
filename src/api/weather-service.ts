0f59deed6cdcaf593dfd8f316f24d2f2fb006161
Victor de Assis <victoralvespda@gmail.com>
2022-09-26 19:35:37 +0200

    Added the weather model, refactored the getGeoData, and started doing the basics of the UI.

--- src/api/weather-service.ts
+++ src/api/weather-service.ts
@@ -1,11 +1,9 @@
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
+import { WeatherModel } from "../models/weather.model";
 
-export const getLocationData = (geoData: GeolocationPosition) => {
+export const getLocationData = (geoData: GeolocationPosition): AxiosPromise<WeatherModel>  => {
     const {latitude, longitude} = geoData.coords
-    axios(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&id=524901&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
-    .then(response => {
-        console.log(response.data);
-      }, error => {
-        console.log(error);
-      });
+
+    return axios(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&id=524901&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
+
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,84 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getLocationData } from '../../api/weather-service';
+import { WeatherModel } from '../../models/weather.model';
 
 const Home: React.FC = () => {
+  const [geoData, setGeoData] = useState<WeatherModel>();
+
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((geoData) =>
-        getLocationData(geoData)
-      );
+      navigator.geolocation.getCurrentPosition((geoData) => {
+        getLocationData(geoData).then(
+          (response) => {
+            console.log('response.data', response.data);
+            setGeoData(response.data);
+          },
+          (error) => {
+            console.log('error', error);
+          }
+        );
+      });
     } else {
       alert('Geolocation is not supported by this browser.');
     }
   }, []);
 
   return (
-    <>
+    <div className="mx-96">
       <div className="text-center mb-10 mt-10">
         <h1 className="font-paytone-one text-3xl ">Do I need an ☂️?</h1>
       </div>
+      <div className="flex justify-start flex-col">
+        <h1 className="text-3xl font-bold">Right now</h1>
+        <h1 className="first-letter:uppercase">
+          {geoData?.weather[0].description} for the hour.
+        </h1>
+        <h1 className="first-letter:uppercase">
+          {`${Math.round(geoData?.wind.speed || 0)}m/s winds from ${
+            geoData?.wind.deg
+          } degrees.`}
+        </h1>
+        <div className="flex">
+          <div>
+            <img
+              src={`http://openweathermap.org/img/wn/${geoData?.weather[0].icon}@2x.png`}
+              width={100}
+            />
+          </div>
+          <div>
+            <h1 className="text-7xl font-bold mt-2">
+              {Math.round(geoData?.main.temp || 0)}°C
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-start flex-col">
+        <h1 className="text-3xl font-bold">This morning</h1>
+        <h1 className="first-letter:uppercase">
+          {geoData?.weather[0].description} for the hour.
+        </h1>
+        <h1 className="first-letter:uppercase">
+          {`${Math.round(geoData?.wind.speed || 0)}m/s winds from ${
+            geoData?.wind.deg
+          } degrees.`}
+        </h1>
+        <div className="flex">
+          <div>
+            <img
+              src={`http://openweathermap.org/img/wn/${geoData?.weather[0].icon}@2x.png`}
+              width={100}
+            />
+          </div>
+          <div>
+            <h1 className="text-7xl font-bold mt-2">
+              {Math.round(geoData?.main.temp || 0)}°C
+            </h1>
+          </div>
+        </div>
+      </div>
+
       <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5">
@@ -30,7 +91,7 @@ const Home: React.FC = () => {
         </div>
       </div>
       <Link to="/about">About</Link>
-    </>
+    </div>
   );
 };
 
